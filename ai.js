baba5bda8f6c1e3b5087ac3bf56222979b20d5c7
Taylor Page <jonathantaylorpage@gmail.com>
2016-12-19 21:44:38 -0600

    Create naive question detector

--- ai.js
+++ ai.js
@@ -1,6 +1,15 @@
-let text = document.getElementById('request')
-let button = document.getElementById('button')
-
+//INITIATE TEXT ANALYSIS
 button.onclick = () => {
+	let text  = document.getElementById('request').value.toLowerCase()
+	let button = document.getElementById('button')
 
+	let results = 'statement';
+	let question_key_words = ['who', 'what', 'where', 'when', 'why', 'how']
+
+	text.split(' ').forEach((word) => {
+		if (question_key_words.indexOf(word) > -1 || word.indexOf('?') > -1) {
+			results = 'question';
+		}
+	});
+	console.log(results);
 }
